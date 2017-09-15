90b711d635f8c3bfd52254f1204b9ccd12bf77a3
Voldymyr Hil <vhi@cabana.dk>
2017-09-15 15:15:10 +0300

    gulp deploy

---
+++ gulp-config.js
@@ -0,0 +1,8 @@
+module.exports = function () {
+  var config = {
+  	websiteRoot: "F:\\Projects\\SugAnalytics\\Deploy\\Website",
+    buildConfiguration: "Debug",
+    runCleanBuilds: false
+  }
+  return config;
+}
